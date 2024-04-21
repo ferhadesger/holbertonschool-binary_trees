4548e1021615d822c34a0fb9842583821e58116b
ferhadesger <ferhadesgerzade14@gmail.com>
2024-04-21 10:53:07 +0000

    betty bug fixed

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-if (tree == NULL)
-return (0);
-if (tree->left == NULL && tree->right == NULL)
-return (0)
-left = binary_tree_nodes(tree->left);
-right = binary_tree_nodes(tree->right);
-return (left + right + 1);
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->right);
+
+	return (left + right + 1);
 }
